3c859877bebfb8227ca9c3208f1196270531a69c
an90ass <21670310176@ogrenci.bartin.edu.tr>
2024-09-10 15:09:50 +0300

    edited ip

--- Shop Viewer/lib/data_api/catagory_api.dart
+++ Shop Viewer/lib/data_api/catagory_api.dart
@@ -1,9 +1,9 @@
 import 'package:http/http.dart' as http;
 class CategoryApi{
   static Future getCatagories(){
-    return http.get(Uri.parse("http://192.168.1.102:3000/categories"));
+    return http.get(Uri.parse("http://localhost:3000/categories"));
 }
   static Future getCategoryById(int id) {
-    return http.get(Uri.parse("http://192.168.1.102:3000/categories?id=$id"));
+    return http.get(Uri.parse("http://localhost:3000/categories?id=$id"));
   }
 }
--- Shop Viewer/lib/data_api/product_api.dart
+++ Shop Viewer/lib/data_api/product_api.dart
@@ -1,11 +1,11 @@
 import "package:http/http.dart" as http;
 class ProductAapi{
   static Future getProducts(){
-    return http.get(Uri.parse("http://192.168.1.102:3000/products"));
+    return http.get(Uri.parse("http://localhost:3000/products"));
 
   }
   static Future getProductsByCatagoryId(int catagoryId){
-    return http.get(Uri.parse("http://192.168.1.102:3000/products?categoryId=$catagoryId"));
+    return http.get(Uri.parse("http://localhost:3000/products?categoryId=$catagoryId"));
 
 
   }
